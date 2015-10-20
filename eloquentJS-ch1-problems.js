22d46adc5432de732a0ec7ffb0537b3cf46d30b4
Anthony Albertorio <anthony.albertorio@gmail.com>
2015-10-20 17:36:43 -0400

    solved chapter 2 problems, checkerboard, fizzbuzz, output # 8x

---
+++ eloquentJS-ch1-problems.js
@@ -0,0 +1,80 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+var pound = "#";
+for(var i = 1; i < 8; i++){
+    console.log(pound);
+    pound += "#";
+}
+
+
+/*
+Fizz Buzz
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+For numbers divisible by 3, print "Fizz" instead of the number, 
+and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print "FizzBuzz",
+ for numbers that are divisible by both 3 and 5 
+ (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+*/
+
+var upperLimit = 100;
+var lowerLimit = 1;
+for(var i = lowerLimit; i < upperLimit + 1; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+       console.log(i, "Fizz Buzz"); 
+    } else if (i % 3 === 0){
+        console.log(i,"Fizz");
+    } else if(i % 5 === 0){
+        console.log(i, "Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+
+/*
+Write a program that creates a string that represents an 8×8 grid, 
+using newline characters to separate lines. 
+At each position of the grid there is either a space or a “#” character. 
+The characters should form a chess board.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+
+When you have a program that generates this pattern, 
+define a variable size = 8 and change the program so that it works for any size, 
+outputting a grid of the given width and height.
+*/
+
+//checkers board
+function checkerboard(size) {
+    for(var i = 0; i < size; i++){
+        if (i % 2 === 0){
+            console.log(" # # # #");
+        } else {
+           console.log("# # # # "); 
+        }
+    }
+}
+
+checkerboard(8);
